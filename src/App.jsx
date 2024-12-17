5b359275b26e0ea5b4f83099c3bae587219e8f35
Saurabh Kumar Rawat <rawatrawatsourav@gmail.com>
2024-12-17 23:19:34 +0530

    heading added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import  Form from './COMPONENTS/Form'
 
 function App() {
   return (
-    <div className='w-screen h-screen bg-black overflow-scroll'>
+    <div className='w-screen h-screen bg-black overflow-scroll p-6'>
+      <h1 className='text-yellow-500 text-center text-4xl font-bold'>TODO APP</h1>
       <div className='  w-full md:w-[80%] lg:w-[50%]  m-auto p-4'>
       <Form></Form>
       
